3309d0dc5c54b53bb72af41899c236865e9f84c6
Sevban Bayrak <svbnbyrk@gmail.com>
2019-10-30 11:00:17 +0300

    deneme

---
+++ CoreFundamental.Data/IRestaurantData.cs
@@ -0,0 +1,42 @@
+﻿using CoreFundamental.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoreFundamental.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{ Id=1, Location="İzmit",Name="Kavuret", Cousin = CousinType.Turkish  },
+                new Restaurant{ Id=2, Location="İzmit",Name="Burger King" ,Cousin = CousinType.Russian },
+                new Restaurant{ Id=3, Location="Tuzla",Name="Tuzla Balıkçısı" ,Cousin = CousinType.Mexico },
+                new Restaurant{ Id=4, Location="Sakarya",Name="Köfteci Nazmi" ,Cousin = CousinType.Turkish },
+            };
+
+        }
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
---
+++ CoreFundamental/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreFundamental.Core;
+using CoreFundamental.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreFundamental.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+
+        public string Message { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        private readonly IRestaurantData restaurantData;
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+       
+        public void OnGet(string searchTerm)
+        {
+            Message = config["Message"];
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
+        }
+    }
+}
