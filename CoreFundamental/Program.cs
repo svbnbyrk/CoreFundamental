3a3a0d4c161991f2b333dbf8ca294bfb4b5823c4
Sevban Bayrak <svbnbyrk@gmail.com>
2019-11-08 16:37:43 +0300

    core publish db migration options

--- CoreFundamental/Program.cs
+++ CoreFundamental/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreFundamental.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +16,20 @@ namespace CoreFundamental
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            MigrateDatabase(host);
+            
+            host.Run();
+        }
+
+        private static void MigrateDatabase(IHost host)
+        {
+            using(var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<CoreFundamentalDbContext>();
+                db.Database.Migrate();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
